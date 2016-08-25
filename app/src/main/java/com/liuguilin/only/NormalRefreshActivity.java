6b44cb93cd04a2284bc246868a3f524abbc4975e
failweili <fail572872916@163.com>
2016-08-25 18:03:04 +0800

    2016年8月25日 11:42:45

--- app/src/main/java/com/liuguilin/only/NormalRefreshActivity.java
+++ app/src/main/java/com/liuguilin/only/NormalRefreshActivity.java
@@ -3,12 +3,28 @@ package com.liuguilin.only;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.liuguilin.only.adapter.GridAdapter;
+import com.liuguilin.only.bean.GirlBean;
+import com.liuguilin.only.utils.L;
 import com.ybao.pullrefreshview.layout.BaseFooterView;
 import com.ybao.pullrefreshview.layout.BaseHeaderView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +50,7 @@ public class NormalRefreshActivity extends AppCompatActivity implements BaseHead
         listView = (ListView) findViewById(R.id.list);
         headerView = (BaseHeaderView) findViewById(R.id.header);
         footerView = (BaseFooterView) findViewById(R.id.footer);
-
+        getGirl();
         list = getData(15);
 
         adapter = new ArrayAdapter(this, R.layout.item, list);
@@ -67,6 +83,7 @@ public class NormalRefreshActivity extends AppCompatActivity implements BaseHead
             public void run() {
                 page++;
                 List<String> datas = getData(5);
+                getGirl();
                 list.addAll(datas);
                 adapter.notifyDataSetChanged();
                 footerView.stopLoad();
@@ -83,6 +100,57 @@ public class NormalRefreshActivity extends AppCompatActivity implements BaseHead
             datas.add("第" + page + "页,第" + i + "条");
         }
         return datas;
+    }
+    /**
+     * 解析国内新闻数据
+     */
+    private void getGirl(){
+        //聚合的接口
+//        String url = "http://gank.io/api/search/query/listview/category/福利/count/50/page/" + count;
+      String url=  "http://api.avatardata.cn/GuoNeiNews/Query?key=166a772bb5954abab5ed6cc179939ca3&page="+page+"&rows=10";
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    // 成功
+                    @Override
+                    public void onResponse(String json) {
+//                        L.i("json", json);
+                        Volley_news(json);
+                    }
+                }, new Response.ErrorListener() {
+            // 失败
+            @Override
+            public void onErrorResponse(VolleyError errorLog) {
+            }
+        });
+
+        queue.add(request);
+    }
+    /**
+     * json解析
+     *
+     * @param json
+     */
+    private void Volley_news(String json) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            Log.d("NormalRefreshActivity", json);
+
+//            for (int i = 0; i < jsonArray.length(); i++) {
+//                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+//
+//
+//            }
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
